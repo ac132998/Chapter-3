9596ce242cffe02db550a45c3527e690726eda90
ac132998 <ac132998@users.noreply.github.com>
2019-11-21 15:13:28 -0600

    Updated MadLibs

--- MadLibs/MadLibs/Form1.Designer.cs
+++ MadLibs/MadLibs/Form1.Designer.cs
@@ -52,154 +52,174 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(334, 37);
+            this.label1.Font = new System.Drawing.Font("Kristen ITC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(225, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.Size = new System.Drawing.Size(258, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome to Mad Libs";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(77, 88);
+            this.label2.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 80);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.Size = new System.Drawing.Size(77, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "Enter a color";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(77, 122);
+            this.label3.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 114);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.Size = new System.Drawing.Size(150, 15);
             this.label3.TabIndex = 2;
             this.label3.Text = "Enter a word ending in est";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(77, 160);
+            this.label4.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 152);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.Size = new System.Drawing.Size(93, 15);
             this.label4.TabIndex = 3;
             this.label4.Text = "Enter an animal";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(77, 194);
+            this.label5.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(35, 186);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.Size = new System.Drawing.Size(76, 15);
             this.label5.TabIndex = 4;
             this.label5.Text = "Enter a noun";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(77, 227);
+            this.label6.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(389, 150);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(116, 13);
+            this.label6.Size = new System.Drawing.Size(137, 15);
             this.label6.TabIndex = 5;
             this.label6.Text = "Enter a body part plural";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(77, 264);
+            this.label7.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(389, 187);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(94, 13);
+            this.label7.Size = new System.Drawing.Size(108, 15);
             this.label7.TabIndex = 6;
             this.label7.Text = "Enter a pural noun";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(432, 88);
+            this.label8.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(390, 80);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(82, 13);
+            this.label8.Size = new System.Drawing.Size(93, 15);
             this.label8.TabIndex = 7;
             this.label8.Text = "Enter an integer";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(432, 119);
+            this.label9.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(390, 111);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(109, 13);
+            this.label9.Size = new System.Drawing.Size(125, 15);
             this.label9.TabIndex = 8;
             this.label9.Text = "Enter an other integer";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(298, 309);
+            this.button1.BackColor = System.Drawing.Color.Pink;
+            this.button1.Font = new System.Drawing.Font("Castellar", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(254, 222);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 23);
+            this.button1.Size = new System.Drawing.Size(200, 23);
             this.button1.TabIndex = 10;
             this.button1.Text = "Click for story!";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtcolor
             // 
-            this.txtcolor.Location = new System.Drawing.Point(263, 85);
+            this.txtcolor.BackColor = System.Drawing.Color.Pink;
+            this.txtcolor.Location = new System.Drawing.Point(221, 77);
             this.txtcolor.Name = "txtcolor";
             this.txtcolor.Size = new System.Drawing.Size(100, 20);
             this.txtcolor.TabIndex = 11;
             // 
             // txtword
             // 
-            this.txtword.Location = new System.Drawing.Point(263, 119);
+            this.txtword.BackColor = System.Drawing.Color.Pink;
+            this.txtword.Location = new System.Drawing.Point(221, 111);
             this.txtword.Name = "txtword";
             this.txtword.Size = new System.Drawing.Size(100, 20);
             this.txtword.TabIndex = 12;
             // 
             // txtanimal
             // 
-            this.txtanimal.Location = new System.Drawing.Point(263, 153);
+            this.txtanimal.BackColor = System.Drawing.Color.Pink;
+            this.txtanimal.Location = new System.Drawing.Point(221, 145);
             this.txtanimal.Name = "txtanimal";
             this.txtanimal.Size = new System.Drawing.Size(100, 20);
             this.txtanimal.TabIndex = 13;
             // 
             // txtnoun
             // 
-            this.txtnoun.Location = new System.Drawing.Point(263, 191);
+            this.txtnoun.BackColor = System.Drawing.Color.Pink;
+            this.txtnoun.Location = new System.Drawing.Point(221, 183);
             this.txtnoun.Name = "txtnoun";
             this.txtnoun.Size = new System.Drawing.Size(100, 20);
             this.txtnoun.TabIndex = 14;
             // 
             // txtbodypart
             // 
-            this.txtbodypart.Location = new System.Drawing.Point(263, 227);
+            this.txtbodypart.BackColor = System.Drawing.Color.Pink;
+            this.txtbodypart.Location = new System.Drawing.Point(558, 152);
             this.txtbodypart.Name = "txtbodypart";
             this.txtbodypart.Size = new System.Drawing.Size(100, 20);
             this.txtbodypart.TabIndex = 15;
             // 
             // txtnounplural
             // 
-            this.txtnounplural.Location = new System.Drawing.Point(263, 264);
+            this.txtnounplural.BackColor = System.Drawing.Color.Pink;
+            this.txtnounplural.Location = new System.Drawing.Point(558, 187);
             this.txtnounplural.Name = "txtnounplural";
             this.txtnounplural.Size = new System.Drawing.Size(100, 20);
             this.txtnounplural.TabIndex = 16;
             // 
             // txtnum1
             // 
-            this.txtnum1.Location = new System.Drawing.Point(600, 88);
+            this.txtnum1.BackColor = System.Drawing.Color.Pink;
+            this.txtnum1.Location = new System.Drawing.Point(558, 80);
             this.txtnum1.Name = "txtnum1";
             this.txtnum1.Size = new System.Drawing.Size(100, 20);
             this.txtnum1.TabIndex = 17;
             // 
             // txtnum2
             // 
-            this.txtnum2.Location = new System.Drawing.Point(600, 119);
+            this.txtnum2.BackColor = System.Drawing.Color.Pink;
+            this.txtnum2.Location = new System.Drawing.Point(558, 111);
             this.txtnum2.Name = "txtnum2";
             this.txtnum2.Size = new System.Drawing.Size(100, 20);
             this.txtnum2.TabIndex = 18;
             // 
             // lblstory
             // 
+            this.lblstory.BackColor = System.Drawing.Color.Pink;
             this.lblstory.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblstory.Location = new System.Drawing.Point(129, 360);
+            this.lblstory.Location = new System.Drawing.Point(96, 257);
             this.lblstory.Name = "lblstory";
             this.lblstory.Size = new System.Drawing.Size(510, 80);
             this.lblstory.TabIndex = 20;
@@ -208,6 +228,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Plum;
             this.ClientSize = new System.Drawing.Size(729, 480);
             this.Controls.Add(this.lblstory);
             this.Controls.Add(this.txtnum2);
